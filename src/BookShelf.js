83359e6622fb169c42930d5d5b8e928faedfab6a
Aron Roberts <robotros1@msn.com>
2018-09-26 14:07:34 -0700

    feat: added PropTypes

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,31 +1,51 @@
 import React, {Component} from 'react';
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Book from './Book'
+import PropTypes from 'prop-types'
 
+
+/**
+* React Component to Render a BookShelf
+* @author [Aron Roberts](https://github.com/robotros)
+*/
 class BookShelf extends Component {
-    render() {
-      const shelf ={
-        shelfTitle: this.props.title,
-        books:this.props.books,
-      }
+  render() {
+    const shelf ={
+      shelfTitle: this.props.title,
+      books:this.props.books,
+    }
 
-        
-      return (
-        <div className="bookshelf">
-                  <h2 className="bookshelf-title">{shelf.shelfTitle}</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                       {shelf.books.map((book, index) => (
-                          <li key={index}>
-                            <Book details={book} onUpdateShelf={this.props.onUpdateShelf}/>
-                          </li>))}
-                    </ol>
-                  </div>
-                </div>
-);
-      
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelf.shelfTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {shelf.books.map((book, index) => (
+              <li key={index}>
+                <Book details={book} onUpdateShelf={this.props.onUpdateShelf}/>
+              </li>))
+            }
+          </ol>
+        </div>
+      </div>
+    );
   }
+
+}
+
+BookShelf.propTypes = {
+  /**
+  * Function to Change shelf of a book
+  */
+  onUpdateShelf: PropTypes.func.isRequired,
+  /**
+  * The title of the Book Shelf
+  */
+  title: PropTypes.string.isRequired,
+  /**
+  * Array of Books that belong on the shelf
+  */
+  books: PropTypes.array,
 }
 
 export default BookShelf;
