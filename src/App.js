592a4c747130242e8d4343f54a796d5392adc13b
Aron Roberts <robotros1@msn.com>
2018-09-26 14:00:02 -0700

    style: formating

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import {Link} from 'react-router-dom'
 import {Route} from 'react-router-dom';
 import * as BooksAPI from "./BooksAPI";
 import './App.css';
-
 import BookShelf from './BookShelf';
 import SearchPage from './SearchPage';
 
 class BooksApp extends React.Component {
-   state = {books:[],
-            screen:'',}
+  state = {
+    books:[],
+    screen:'',
+  }
 
   componentDidMount() {
     BooksAPI.getAll().then(data => {
@@ -35,33 +36,33 @@ class BooksApp extends React.Component {
     //console.log(this.state.books);
     return (
       <div className="app">
-       <Route path='/search'  render={()=>(<SearchPage onShelfs={this.state.books} onUpdateShelf={this.handleChangeShelf} />)}/>
-       <Route exact path='/' render={()=> (
-
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-              <BookShelf
-                onUpdateShelf={this.handleChangeShelf}
-                title='Currently Reading'
-                books={this.state.books.filter(b => b.shelf === 'currentlyReading')}/>
-              <BookShelf
-                onUpdateShelf={this.handleChangeShelf}
-                title='Want to Read'
-                books={this.state.books.filter(b => b.shelf === 'wantToRead')}/>
-              <BookShelf
-                onUpdateShelf={this.handleChangeShelf}
-                title='Read'
-                books={this.state.books.filter(b => b.shelf === 'read')}/>
+        <Route path='/search'  render={()=>(<SearchPage onShelfs={this.state.books} onUpdateShelf={this.handleChangeShelf} />)}/>
+        <Route exact path='/' render={()=> (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <BookShelf
+                  onUpdateShelf={this.handleChangeShelf}
+                  title='Currently Reading'
+                  books={this.state.books.filter(b => b.shelf === 'currentlyReading')}/>
+                <BookShelf
+                  onUpdateShelf={this.handleChangeShelf}
+                  title='Want to Read'
+                  books={this.state.books.filter(b => b.shelf === 'wantToRead')}/>
+                <BookShelf
+                  onUpdateShelf={this.handleChangeShelf}
+                  title='Read'
+                  books={this.state.books.filter(b => b.shelf === 'read')}/>
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to='/search' className='Add-book'>Add a book</Link>
             </div>
           </div>
-          <div className="open-search">
-            <Link to='/search' className='Add-book'>Add a book</Link>
-          </div>
-        </div>)}/>
+        )}/>
       </div>
     )
   }
