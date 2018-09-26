26d65689af477e0586366463e58649a505c97df5
Aron Roberts <robotros1@msn.com>
2018-09-26 13:24:21 -0700

    feat: add proptypes

--- src/Author.js
+++ src/Author.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
 import './App.css';
+import PropTypes from 'prop-types'
 
+
+/**
+* React Component to Render a Book's Authors
+* @author [Aron Roberts](https://github.com/robotros)
+*/
 class Author extends Component {
-    render() {
-      const authors = this.props.author;
-      
-      return (
-        <div>
-        {authors ? authors.map((a, index) => (<div key={index} className="book-authors">{a}</div>)) : <div></div>}
-       </div>
-      )
-    }
+  render() {
+    const authors = this.props.author;
+    return (
+      <div>
+        {authors ? authors.map((a, index) => (<div key={index} className='book-authors'>{a}</div>)) : <div></div>}
+      </div>
+    )
+  }
+}
+
+Author.propTypes = {
+  /**
+  * Array of Authors for a book
+  */
+  authors: PropTypes.array,
 }
 
 export default Author;
