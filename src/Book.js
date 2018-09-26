34e19b0610c6a7d4e125ba60163bc5677d19fae0
Aron Roberts <robotros1@msn.com>
2018-09-26 11:54:32 -0700

    feat: add placeholder image

--- src/Book.js
+++ src/Book.js
@@ -6,19 +6,20 @@ import Author from './Author'
 class Book extends Component {
     render() {
       const details = this.props.details
-                    
+      const noCoverArt ='http://lgimages.s3.amazonaws.com/nc-md.gif'
+
       return (
         <div className="book">
           <div className="book-top">
-            <img className="book-cover" alt={details.title} src={details.imageLinks.thumbnail} ></img>
+            <img className="book-cover" alt={details.title} src={details.imageLinks ? details.imageLinks.thumbnail : noCoverArt} ></img>
            <BookShelfChanger book={details} onUpdateShelf={this.props.onUpdateShelf}/>
       </div>
       <div className="book-title">{details.title}</div>
       <Author author={details.authors}/>
-      
+
   </div>
 );
-      
+
   }
 }
 
